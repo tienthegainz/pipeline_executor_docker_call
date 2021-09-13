bea6eb77d9312d2809b26267f05be9a53e1fb8e5
Tien <viettienha98@gmail.com>
2021-09-13 11:02:16 +0700

    Add exc, caller

---
+++ caller/main.py
@@ -0,0 +1,48 @@
+from __future__ import print_function, unicode_literals
+import random
+import time
+import docker
+import re
+from pprint import pprint
+from PyInquirer import prompt, print_json
+
+
+docker_client = docker.from_env()
+
+
+def main():
+  images_list = docker_client.images.list(all=True)
+  questions = [
+    {
+        'type': 'list',
+        'name': 'image',
+        'message': 'Which image is used to execute?',
+        'choices': [
+          'ID: {id} - Tags: {tags}'.format(id=image.short_id, tags=image.tags) for image in images_list if image.tags
+        ],
+        'filter': lambda val: re.search(r"sha256:\w*", val).group(0)
+    }, 
+    {
+        'type': 'list',
+        'name': 'task',
+        'message': 'Which task is executed?',
+        'choices': ['Dataset 1', 'Dataset 2', 'Dataset 3'],
+    },
+    {
+        'type': 'confirm',
+        'message': 'Run container in detach mode?',
+        'name': 'detach',
+        'default': True,
+    },
+  ]
+  answers = prompt(questions)
+  pprint(answers)
+
+  c = docker_client.containers.run(answers['image'], 'python3 main.py --task=\"{}\"'.format(answers['task']), detach=answers['detach'])
+  if answers['detach']:
+    print('Container {} is running. Use docker logs to view'.format(c.short_id))
+  else:
+    print('Container {} finished'.format(c.short_id))
+
+if __name__ == "__main__":
+  main()
---
+++ executors/exc2/main.py
@@ -0,0 +1,18 @@
+import random
+import time
+from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--task', type=str, default='ImageNet', help='example argument')
+
+def main(task: str):
+  for i in tqdm(range(180)):
+    time.sleep(1)
+  
+  acc = random.random()
+  print('Exc2 finished task {task} ==> Acc: {acc: .2f}'.format(task=task, acc = acc))
+
+if __name__ == "__main__":
+  args = parser.parse_args()
+  main(args.task)
